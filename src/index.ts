d6126b9abad56f0424af0d607815f856374854da
09step <stepan.vedunov@zhdk.ch>
2025-11-04 08:29:30 +0100

    splitting up monorepo

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  //for dev only:
+  // origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+app.use(express.json());
+
+app.get('/health', (req: Request, res: Response) => {
+  res.json({ status: 'ok', timestamp: new Date().toISOString() });
+});
+
+app.post('/api/log', (req: Request, res: Response) => {
+  const { action } = req.body;
+  console.log('Received:', action);
+  console.log(process.env.FRONTEND_URL)
+  res.json({ action });
+});
+
+app.listen(PORT, () => {
+  console.log(`Port: ${PORT}`);
+});
