06b5e18cbdd7e8008c8ba2fdcd4bfbee86e29760
09step <stepan.vedunov@zhdk.ch>
2025-12-05 15:37:52 +0100

    fix: add Query to fastapi import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, BackgroundTasks
+from fastapi import FastAPI, HTTPException, BackgroundTasks, Query
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, field_validator
 from datetime import datetime, timedelta
